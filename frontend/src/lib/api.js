9fe4a96f3fcac5faa7a93fc8df5ec1ec4e2afd90
samantha-bekantan <samantha@bekantan.dev>
2026-08-15 19:36:45 +0200

    Revert accidental lib update

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -22,17 +22,5 @@ api.interceptors.response.use(
     return Promise.reject(err.response?.data || err);
   }
 );
-import { useAuthStore } from '../store/authStore';
-
-api.interceptors.response.use(
-  (res) => res.data,
-  (err) => {
-    if (err.response?.status === 401) {
-      useAuthStore.getState().logout();
-      window.location.href = '/login';
-    }
-    return Promise.reject(err.response?.data || err);
-  }
-);
 
 export default api;
